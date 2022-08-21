a9eef7ed40fb80ba7b9620d28c542f8a3d00877c
MashaMartin <59993828+MashaMartin@users.noreply.github.com>
2022-08-21 20:11:36 +0200

    Add files via upload

---
+++ src/ternaryclass.java
@@ -0,0 +1,16 @@
+
+public class ternaryclass {
+
+	public static void main(String[] args) {
+		// Ternary condition
+		int one =1;
+		int two = 2;
+		int result = one > two
+					? (one - two)
+					: (two - one);
+		
+		System.out.println(result);
+
+	}
+
+}
